f96a183a66cfea6484f6f01df0d9a897deb40646
filipecsoares <filipecarlossoares@gmail.com>
2024-02-07 20:31:09 -0300

    refactor: refactor cpf validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,65 +1,39 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
 
-export function validateCpf (str: string) {
-	if (str !== null) {
-		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14){
-				// cleaning cpf
-				str=str
-					.replace('.','')
-					.replace('.','')
-					.replace('-','')
-					.replace(" ","");  
-	
-				if (!str.split("").every(c => c === str[0])) {
-					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
-						d1 = d2 = 0;  
-						dg1 = dg2 = rest = 0;  
-							
-						for (let nCount = 1; nCount < str.length -1; nCount++) {  
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
-	
-								digito = parseInt(str.substring(nCount -1, nCount));  							
-								d1 = d1 + ( 11 - nCount ) * digito;  
-				
-								d2 = d2 + ( 12 - nCount ) * digito;  
-							// }
-						};  
-							
-						rest = (d1 % 11);  
-				
-						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-						d2 += 2 * dg1;  
-						rest = (d2 % 11);  
-						if (rest < 2)  
-							dg2 = 0;  
-						else  
-							dg2 = 11 - rest;  
-				
-							let nDigVerific = str.substring(str.length-2, str.length);  
-						nDigResult = "" + dg1 + "" + dg2;  
-						return nDigVerific == nDigResult;
-						
-					// just in case...
-					}catch (e){  
-						console.error("Erro !"+e);  
-	
-						return false;  
-					}  
-				} else return false
-	
-			}else return false;
-		}
+export function validateCpf (cpf: string) {
+	if (!cpf) return false;
+	cpf = cleanNonDigit(cpf);
+	if (isInvalidLength(cpf)) return false;
+	if (allDigitsAreTheSame(cpf)) return false;
+	const dg1 = calculateDigit(cpf, 10);
+	const dg2 = calculateDigit(cpf, 11);
+	return extractCheckDigit(cpf) === `${dg1}${dg2}`;
+}
 
+function cleanNonDigit (cpf: string) {
+	return cpf.replace(/\D/g, "");
+}
 
-	} else return false;
+function isInvalidLength (cpf: string) {
+	return cpf.length !== 11;
+}
+
+function allDigitsAreTheSame (cpf: string) {
+	return cpf.split("").every(c => c === cpf[0]);
+}
+
+function calculateDigit (cpf: string, factor: number) {
+	let total = 0;
+	for (const digit of cpf) {
+		if (factor > 1) total += parseInt(digit) * factor--;
+	}
+	const rest = total%11;
+	return (rest < 2) ? 0 : 11 - rest;
+}
+
+function extractCheckDigit (cpf: string) {
+	return cpf.slice(9);
 }
 
 export async function signup (input: any): Promise<any> {
